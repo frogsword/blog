48edffb05af5d5ad337c94d18e0d49a27bff3164
frogsword <113356699+frogsword@users.noreply.github.com>
2024-01-18 13:38:15 -0600

    Update post.js
    added autopopulate to author field

--- api/models/post.js
+++ api/models/post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
     title: {type: String, minLength: 1, required: true},
     text: {type: String, minLength: 1, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'User', required: true, autopopulate: true},
     likes: [{type: Schema.Types.ObjectId, ref: 'User'}],
     comments: [{type: Schema.Types.ObjectId, ref: 'Comment', autopopulate: true}],
     dateCreated: {type: Date, default: Date.now},
