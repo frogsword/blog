c0a5ce01ee856e875e4a86db6971fa124779cdf7
frogsword <113356699+frogsword@users.noreply.github.com>
2024-01-20 05:10:01 -0600

    Update comment.js

--- api/models/comment.js
+++ api/models/comment.js
@@ -5,7 +5,7 @@ const CommentSchema = new Schema({
     comment: {type: String, minLength: 1, required: true},
     post: {type: Schema.Types.ObjectId, ref: 'Post'},
     parentComment: {type: Schema.Types.ObjectId, ref: 'Comment'},
-    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true, autopopulate: true},
     comments: [{type: Schema.Types.ObjectId, ref: 'Comment', autopopulate: true}],
     likes: [{type: Schema.Types.ObjectId, ref: 'User'}],
     dateCreated: {type: Date, default: Date.now},
